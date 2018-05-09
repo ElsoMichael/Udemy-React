6b906e1082828f2397a0e5659aacd6abeee3d7f5
ElsoMichael <elsomichael@ymail.com>
2018-05-09 10:27:40 -0400

    Finding Logistical Error

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id
+      return p.id === id // p.userID === id
     });
 
     const person = {
