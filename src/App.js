bd07b791501fe5154a67a6109b6e46f63263a4d7
ElsoMichael <elsomichael@ymail.com>
2018-05-09 10:23:29 -0400

    Understanding Error Messages

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
 
     // const person = Object.assign({}, this.state.persons[personIndex]);
 
-    person.name = event.target.value;
+    person.name = event.target.value; // event.input.value
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
