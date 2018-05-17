b0c56ad16b4dc146d7e6aa20eda9fdc43311db86
ElsoMichael <elsomichael@ymail.com>
2018-05-17 11:56:00 -0400

    Converting Statless to Statful

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
 
-const person = (props) => {
-  const rnd = Math.random();
-
-    if (rnd > 0.7) {
-      throw new Error("Something Went Wrong");
-    }
-    
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} year's old!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+class Person extends Component {
+  render() {
+    return(
+      <div className={classes.Person}>
+      <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} year's old!</p>
+      <p>{this.props.children}</p>
+      <input type="text" onChange={this.props.changed} value={this.props.name}/>
     </div>
-  )
-};
+    )
+  }
+}
 
-export default person;
+export default Person;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => (
-  props.persons.map((person, index) => {
-    return <Person 
-      click={props.click(index)} 
-      name={person.name} 
-      age={person.age} 
-      key={person.id} 
-      changed={(event) => props.changed(event, person.id)} />
-  })
-);
+class Persons extends Component {
+  render() {
+    return (
+      props.persons.map((person, index) => {
+        return <Person 
+          click={this.props.click(index)} 
+          name={person.name} 
+          age={person.age} 
+          key={person.id} 
+          changed={(event) => this.props.changed(event, person.id)} />
+      })
+    )
+  }
+}
 
 export default Persons;
