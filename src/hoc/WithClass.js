6856d146662d5262628677b08cb3b1b52d7b9309
ElsoMichael <elsomichael@ymail.com>
2018-06-01 21:30:31 -0400

    Fix withClass

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const withClass = (WrappedComponent, className) => {
   const withClass = class extends Component {
-    render() {
+    render(props) {
       return (
         <div className={className}>
           <WrappedComponent ref={props.forwardedRef} {...this.props}/>
